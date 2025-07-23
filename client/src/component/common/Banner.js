3ef726fbc67dab6b93edeed8e474fce3b202c553
CongSon2003 <sonchuan1@gmail.com>
2025-07-23 18:01:08 +0700

    Lưu lại thay đổi trong Banner.js

--- client/src/component/common/Banner.js
+++ client/src/component/common/Banner.js
@@ -1,11 +1,41 @@
-import React, { memo } from 'react'
 
-const Banner = () => {
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import './styles.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+export default function App() {
   return (
-    <div className='w-full'>
-      <img src='https://digital-world-2.myshopify.com/cdn/shop/files/slideshow3-home2_1920x.jpg?v=1613166679' alt='banner' className='w-full h-[470px] object-cover' />
-    </div>
-  )
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay,Pagination, Navigation]}
+        className="mySwiper"  
+      >
+        <SwiperSlide>
+          <img src='https://digital-world-2.myshopify.com/cdn/shop/files/promo-20_7441c713-b8bc-4549-b169-67001e3b91e1_1920x.png?v=1750840298' className='object-contain' alt=''/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img alt='' src='https://digital-world-2.myshopify.com/cdn/shop/files/promo-21_94c561f6-4c50-4a5f-8868-0c7b804bc550_1920x.png?v=1750840379'/>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
-
-export default memo(Banner)
