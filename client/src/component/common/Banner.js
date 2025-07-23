2af33665e085ff73bbdf948683c75b54a1d19f25
CongSon2003 <sonchuan1@gmail.com>
2025-07-23 17:57:00 +0700

    Update

--- client/src/component/common/Banner.js
+++ client/src/component/common/Banner.js
@@ -1,16 +1,16 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // import './styles.css';
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 export default function App() {
   return (
@@ -26,14 +26,21 @@ export default function App() {
           clickable: true,
         }}
         navigation={true}
-        modules={[Autoplay,Pagination, Navigation]}
-        className="mySwiper"  
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
       >
         <SwiperSlide>
-          <img src='https://digital-world-2.myshopify.com/cdn/shop/files/promo-20_7441c713-b8bc-4549-b169-67001e3b91e1_1920x.png?v=1750840298' className='object-contain' alt=''/>
+          <img
+            src="https://digital-world-2.myshopify.com/cdn/shop/files/promo-20_7441c713-b8bc-4549-b169-67001e3b91e1_1920x.png?v=1750840298"
+            className="object-contain"
+            alt=""
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <img alt='' src='https://digital-world-2.myshopify.com/cdn/shop/files/promo-21_94c561f6-4c50-4a5f-8868-0c7b804bc550_1920x.png?v=1750840379'/>
+          <img
+            alt=""
+            src="https://digital-world-2.myshopify.com/cdn/shop/files/promo-21_94c561f6-4c50-4a5f-8868-0c7b804bc550_1920x.png?v=1750840379"
+          />
         </SwiperSlide>
       </Swiper>
     </>
