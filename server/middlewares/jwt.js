423c6a0bbe3fc626926a76b6f0e21423046d8e67
CongSon2003 <sonchuan1@gmail.com>
2025-07-23 18:34:41 +0700

    Update

--- server/middlewares/jwt.js
+++ server/middlewares/jwt.js
@@ -4,6 +4,7 @@ const generateAccessToken = (uid, role) => {
   return jwt.sign({_id : uid, role}, process.env.JWT_SECRET, {expiresIn : '2d'})
 }
 
+
 const generateRefreshToken = (uid) => {
   return jwt.sign({_id : uid}, process.env.JWT_SECRET, {expiresIn : '5d'})
 }
