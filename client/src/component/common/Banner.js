311e15ddd73b7f1bce8c21b10e30ae8b0b485772
CongSon2003 <sonchuan1@gmail.com>
2025-07-23 19:07:13 +0700

    Update

--- client/src/component/common/Banner.js
+++ client/src/component/common/Banner.js
@@ -5,10 +5,6 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
