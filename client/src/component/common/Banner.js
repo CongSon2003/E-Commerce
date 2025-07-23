c77e916a92e29eabf27d22f189692cb31f03b22a
CongSon2003 <sonchuan1@gmail.com>
2025-07-23 18:33:51 +0700

    OJ

--- client/src/component/common/Banner.js
+++ client/src/component/common/Banner.js
@@ -5,6 +5,13 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import './styles.css';
+
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 export default function App() {
